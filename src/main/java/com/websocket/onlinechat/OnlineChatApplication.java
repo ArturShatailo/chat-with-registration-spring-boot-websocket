c34edcdbb54cf325eaaab31f337364807cccbf99
Arthur <arturshatailo@gmail.com>
2023-01-15 16:53:36 +0200

    Initial commit

---
+++ src/main/java/com/websocket/onlinechat/OnlineChatApplication.java
@@ -0,0 +1,13 @@
+package com.websocket.onlinechat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineChatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/websocket/onlinechat/OnlineChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.websocket.onlinechat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
