9db31ffd34f04574ec33f9cec1ab89eebbd36d9c
Arthur <arturshatailo@gmail.com>
2023-02-27 18:50:44 +0200

    configurations update

--- src/main/java/com/websocket/onlinechat/authentification/util/exceptions/ExceptionDetails.java
+++ src/main/java/com/websocket/onlinechat/authentification/util/exceptions/ExceptionDetails.java
@@ -1,6 +1,20 @@
 package com.websocket.onlinechat.authentification.util.exceptions;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import java.util.Date;
 
-public record ExceptionDetails(Date timestamp, String message, String details) {
+//public record ExceptionDetails(Date timestamp, String message, String details) {
+//}
+
+@AllArgsConstructor
+@Data
+public class ExceptionDetails {
+
+    private Date timestamp;
+
+    private String message;
+
+    private String details;
+
 }
--- src/main/java/com/websocket/onlinechat/authentification/util/exceptions/FieldExceptionDetails.java
+++ src/main/java/com/websocket/onlinechat/authentification/util/exceptions/FieldExceptionDetails.java
@@ -1,6 +1,22 @@
 package com.websocket.onlinechat.authentification.util.exceptions;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import java.util.Date;
 
-public record FieldExceptionDetails(Date timestamp, String message, String details, String field) {
+//public record FieldExceptionDetails(Date timestamp, String message, String details, String field) {
+//}
+
+@AllArgsConstructor
+@Data
+public class FieldExceptionDetails {
+
+    private Date timestamp;
+
+    private String message;
+
+    private String details;
+
+    private String field;
+
 }
