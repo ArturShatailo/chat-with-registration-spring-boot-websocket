ba327ab345418ce430c501a962d5064482bc895b
Arthur <arturshatailo@gmail.com>
2023-02-27 18:05:48 +0200

    configurations update

--- src/main/java/com/websocket/onlinechat/authentification/email/EmailSender.java
+++ src/main/java/com/websocket/onlinechat/authentification/email/EmailSender.java
@@ -4,7 +4,7 @@ public interface EmailSender {
 
     String FROM_EMAIL = "support@powerfulpanda.tech";
     String SUBJECT = "Confirm your email";
-    String LINK = "http://localhost:8095/api/registration/confirm?token=";
+    String LINK = "https://chat-mate.herokuapp.com/api/registration/confirm?token="; //"http://localhost:8095/api/registration/confirm?token="; //"http://chat-mate.us-east-1.elasticbeanstalk.com/api/registration/confirm?token=";
 
     void send(String to, String email);
 
